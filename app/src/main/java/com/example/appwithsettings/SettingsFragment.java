5c1ad2906aa17e1cc8b2f8062baf5ac45d4381b6
Zi Wan <yipzw-wm17@student.tarc.edu.my>
2019-07-13 01:30:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/appwithsettings/SettingsFragment.java
@@ -0,0 +1,27 @@
+package com.example.appwithsettings;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+
+
+}
